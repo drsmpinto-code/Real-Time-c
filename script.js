b9d176ec88e8573a153e32d8a0d275d16e77005e
drsmpinto-code <drsmpinto@gmail.com>
2025-07-16 22:55:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('https://drsmpinto-code.github.io/Real-Time-c/');
 const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
