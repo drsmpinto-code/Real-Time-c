b9a9bf632dea17c899e13670c2f6e9aafc217faf
drsmpinto-code <drsmpinto@gmail.com>
2025-07-16 22:50:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(3000, {
     cors: {
-        origin: "http://127.0.0.1:5500", // ou "*" para qualquer origem (não recomendado em produção)
+        origin: "*", // ou "*" para qualquer origem (não recomendado em produção)
         methods: ["GET", "POST"]
     }
 });
